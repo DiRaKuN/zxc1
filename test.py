fc1ec0f8494c5d17191826658c447994c714c60e
ваше имя <=>
2021-08-04 19:17:11 +0600

    Продублировал код

--- test.py
+++ test.py
@@ -10,3 +10,30 @@ for k in range(size):
 	text += word1[k] + word2 [-k-1]
 
 print(text)
+
+
+word1 = 'Hello'
+word2 = 'World'
+print(word1 + word2)
+
+text = ''
+size = min(len(word1), len(word2))
+for k in range(size):
+	#print(word1[k]) + word2[-k-1]
+	#print(-k-1)
+	text += word1[k] + word2 [-k-1]
+
+print(text)
+
+word1 = 'Hello'
+word2 = 'World'
+print(word1 + word2)
+
+text = ''
+size = min(len(word1), len(word2))
+for k in range(size):
+	#print(word1[k]) + word2[-k-1]
+	#print(-k-1)
+	text += word1[k] + word2 [-k-1]
+
+print(text)
